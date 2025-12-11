202f19ee1769946d04b4b249c7f1a879dffdba9c
Robin Mattern <robin.mattern@gmail.com>
2025-12-11 10:15:37 -0500

    .(51211.01_Added ENV_FILE correctly

--- _config.yaml.js
+++ _config.yaml.js
@@ -2,5 +2,5 @@ FVARS = {
  "PROJECT_NO":         "55",
  "PROJECT_NAME":       "SAS",
  "PROJECT_VERSION":    "1.02",
- "ENV_FILE":           ".env.remote" 
+ "ENV_FILE":           ".env-remote" 
   }
